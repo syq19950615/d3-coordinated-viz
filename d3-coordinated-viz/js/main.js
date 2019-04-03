6afe0ddc2eb1e3ea7bf4d0cca88a733ebe19240b
syq19950615 <47092414+syq19950615@users.noreply.github.com>
2019-04-03 14:47:38 -0500

    Module 8 assignment completed
    fixed error

--- d3-coordinated-viz/js/main.js
+++ d3-coordinated-viz/js/main.js
@@ -19,8 +19,8 @@ function setMap(){
 
     //create Albers equal area conic projection centered on France
     var projection = d3.geoAlbers()
-        .center([9, 50])
-        .rotate([105, 11])
+        .center([2, 39])
+        .rotate([98, 0])
         .parallels([45, 45])
         .scale(1000)
         .translate([width / 2, height / 2]);
@@ -37,16 +37,17 @@ function setMap(){
     function callback(data){
 	csvData = data[0];
 	states = data[1];
+	
         //Example 2.5 line 3...create graticule generator
         var graticule = d3.geoGraticule()
             .step([10, 10]); //place graticule lines every 5 degrees of longitude and latitude
 
-        //create graticule background
+		//create graticule background
         var gratBackground = map.append("path")
             .datum(graticule.outline()) //bind graticule background
             .attr("class", "gratBackground") //assign class for styling
-            .attr("d", path) //project graticule
-			console.log(gratBackground);
+            .attr("d", path); //project graticule
+			
         //Example 2.6 line 5...create graticule lines
         var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
             .data(graticule.lines()) //bind graticule lines to each element to be created
@@ -54,14 +55,15 @@ function setMap(){
             .append("path") //append each element to the svg as a path element
             .attr("class", "gratLines") //assign class for styling
             .attr("d", path); //project graticule lines		
-		
+
 		//translate states TopoJSON
 		var US_states = topojson.feature(states, states.objects.Export_Output).features;
-		console.log(US_states);
+
 		//examine the results
 		//console.log(europeCountries);
         //console.log(csvData);
         //console.log(US_states);
+		
         //add France regions to map
         var regions = map.selectAll(".regions")
             .data(US_states)
@@ -71,7 +73,6 @@ function setMap(){
                 return "regions " + d.properties.adm1_code;
             })
             .attr("d", path);
-		console.log(regions)
     };
 };
 
